4e70ec56acbb5fd49419d6614b49e0c30d7260c6
tonyjhuang <huangtonyj91@gmail.com>
2019-01-10 09:35:44 -0800

    Combine all components (ButtonsIndex, ResultsIndex, Button) & giphy API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
+import giphy from './api/giphy';
+import ButtonsIndex from './components/ButtonsIndex';
+import ResultsIndex from './components/ResultsIndex';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+      fetchedResults: []
+    }
+  }
+
+  onButtonClick = async (searchTerm) => {
+    const response = await giphy.get('/v1/gifs/search', {
+      params: {
+        q: searchTerm
+      }
+    });
+
+    this.setState({
+      searchTerm: searchTerm,
+      fetchedResults: response.data.data
+    })
+  }
+
   render() {
+    console.log(this.state.fetchedResults.length);
+    
     return (
       <div className="App">
-        HELLO THERE
+        <ButtonsIndex
+          onButtonClick={this.onButtonClick}
+        />
+        <ResultsIndex
+          fetchedResults={this.state.fetchedResults}
+        />
       </div>
     );
   }
